26f7ceaea93d32ac783eba75edf312d100a92850
startFromHere <liutao900629@gmail.com>
2024-07-30 20:02:20 +0800

    Time: 48 ms (9.07%), Space: 16.6 MB (44.1%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -6,7 +6,7 @@ class Solution:
         i, j = 0, 0
         while j < n:
             if nums1[i] > nums2[j] or i - j == m:
-                nums1[i+1:] = nums1[i:m+n-1]
+                nums1[i+1:] = nums1[i:-1]
                 nums1[i] = nums2[j]
                 j += 1
             i += 1
