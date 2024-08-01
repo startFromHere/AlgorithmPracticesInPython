ccd6972e73cc1d18e87c35da2daba387b0bfb2c5
startFromHere <liutao900629@gmail.com>
2024-08-01 22:15:07 +0800

    Time: 47 ms (53.42%), Space: 16.6 MB (32.17%) - LeetHub

--- 2727-number-of-senior-citizens/2727-number-of-senior-citizens.py
+++ 2727-number-of-senior-citizens/2727-number-of-senior-citizens.py
@@ -1,3 +1,4 @@
 class Solution:
     def countSeniors(self, details: List[str]) -> int:
-        return len(list(filter(lambda x: int(x[11: 13]) > 60, details)))
+        # return len(list(filter(lambda x: int(x[11: 13]) > 60, details)))
+        return len([x for x in details if int(x[11:13]) > 60])
