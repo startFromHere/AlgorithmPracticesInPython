bc17fb6e0d4f5d34f37397c04f853f68868ff197
startFromHere <liutao900629@gmail.com>
2024-08-03 21:17:15 +0800

    Time: 157 ms (61.59%), Space: 28 MB (21.5%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -6,11 +6,8 @@ class Solution:
         n = len(nums)
         k = k % n
         
-        # for i in range(k):
-        #     last = nums[-1]
-        #     nums[1:] = nums[:-1]
-        #     nums[0] = last
-        tmp = nums[-k:]+nums[:n-k]
-        for i in range(n):
-            nums[i] = tmp[i]
-        
+        # tmp = nums[-k:]+nums[:n-k]
+        # for i in range(n):
+        #     nums[i] = tmp[i]
+        if k > 0:
+            nums[:k], nums[k:] = nums[-k:], nums[:-k]
