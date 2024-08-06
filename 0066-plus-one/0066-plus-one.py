ba51012e6d4c0fb228e98a4aed29060a827a8647
startFromHere <liutao900629@gmail.com>
2024-08-06 17:39:53 +0800

    Time: 39 ms (39.64%), Space: 16.4 MB (91.13%) - LeetHub

--- 0066-plus-one/0066-plus-one.py
+++ 0066-plus-one/0066-plus-one.py
@@ -9,8 +9,9 @@ class Solution:
             while i >= 0 and ans[i] == 9:
                 ans[i] = 0
                 i -= 1
-            if i < 0:
-                ans.insert(0, 1)
-            else:
+                if i < 0:
+                    ans.insert(0, 1)
+                    break
+            if i >= 0:
                 ans[i] += 1
         return ans
