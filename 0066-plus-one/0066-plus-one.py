8473e8585046f5c4c05251e4d49ebc1f5b57ffa9
startFromHere <liutao900629@gmail.com>
2024-08-06 17:38:06 +0800

    Time: 39 ms (39.64%), Space: 16.4 MB (60.94%) - LeetHub

--- 0066-plus-one/0066-plus-one.py
+++ 0066-plus-one/0066-plus-one.py
@@ -3,12 +3,14 @@ class Solution:
         ans = digits
         n = len(digits)
         i = n-1
-        while i >= 0 and ans[i] == 9:
-            ans[i] = 0
-            i -= 1
-        if i < 0:
-            ans.insert(0, 1)
-        else:
+        if digits[i] < 9:
             ans[i] += 1
+        else:
+            while i >= 0 and ans[i] == 9:
+                ans[i] = 0
+                i -= 1
+            if i < 0:
+                ans.insert(0, 1)
+            else:
+                ans[i] += 1
         return ans
-        
