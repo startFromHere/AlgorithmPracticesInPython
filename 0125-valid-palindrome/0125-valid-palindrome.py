b7175807ef80f43101621eacf11bad97468920b1
startFromHere <liutao900629@gmail.com>
2024-08-20 17:56:06 +0800

    Time: 42 ms (72.91%), Space: 17.3 MB (41.09%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,6 +1,8 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        s = "".join(filter(lambda x: x.isalpha() or x.isdigit(), s)).lower()
+        # s = "".join(filter(lambda x: x.isalpha() or x.isdigit(), s)).lower()
+
+        s = "".join([x for x in s if x.isalpha() or x.isdigit()]).lower()
 
         l, r = 0, len(s)-1
         while l < r:
