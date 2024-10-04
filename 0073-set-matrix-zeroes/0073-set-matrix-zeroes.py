ea06cd11f00890e2b30db747e2d22e5f9f963c90
startFromHere <liutao900629@gmail.com>
2024-10-04 22:23:48 +0800

    Time: 106 ms (37.58%), Space: 17.5 MB (61.1%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -3,27 +3,38 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        aSet = set()
+        # aSet = set()
+        # m, n = len(matrix), len(matrix[0])
+
+        # def handle(i, j):
+        #     for k in range(n):
+        #         handleAdjacent(i, k)
+        #     for k in range(m):
+        #         handleAdjacent(k, j)
+
+        # def handleAdjacent(i, j):
+        #     if matrix[i][j] == 0:
+        #         return
+        #     if i >= 0 and i < m and j >= 0 and j < n:
+        #         matrix[i][j] = 0
+        #     aSet.add(i*n + j)
+
+        # for i in range(m):
+        #     for j in range(n):
+        #         if i*n+j in aSet:
+        #             continue
+        #         if matrix[i][j] == 0:
+        #             handle(i, j)
+        
+        rows, columns = set(), set()
         m, n = len(matrix), len(matrix[0])
-
-        def handle(i, j):
-            print("000i, j:", i, j)
-            for k in range(n):
-                handleAdjacent(i, k)
-            for k in range(m):
-                handleAdjacent(k, j)
-
-        def handleAdjacent(i, j):
-            if matrix[i][j] == 0:
-                return
-            if i >= 0 and i < m and j >= 0 and j < n:
-                matrix[i][j] = 0
-            aSet.add(i*n + j)
-
         for i in range(m):
             for j in range(n):
-                if i*n+j in aSet:
-                    continue
                 if matrix[i][j] == 0:
-                    handle(i, j)
+                    rows.add(i)
+                    columns.add(j)
         
+        for i in range(m):
+            for j in range(n):
+                if i in rows or j in columns:
+                    matrix[i][j] = 0
