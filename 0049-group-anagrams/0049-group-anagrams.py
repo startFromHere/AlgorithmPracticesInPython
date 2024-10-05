518eba2aa26ed4c8f352be45179a35fd21f8bc13
startFromHere <liutao900629@gmail.com>
2024-10-05 22:23:55 +0800

    Time: 88 ms (65.23%), Space: 20.6 MB (41.1%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,12 +1,10 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         aMap = defaultdict(list)
+        ans = []
         for s in strs:
             key = str(sorted(s))
             aMap[key].append(s)
-        ans = []
-        for key in aMap:
-            ans.append(aMap[key])
-        return ans
+        return aMap.values()
 
         
